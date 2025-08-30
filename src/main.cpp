5276cea094d761e8fe0d2f27a3f740de2dae3e41
ppour-ba <pouriya.pourbahrami@gmail.com>
2025-08-31 01:21:28 +0200

    feat(app): integrate temp config parser in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include "TcpServerController.hpp"
+#include "config_temp/temp_pars_config.hpp"
+#include <iostream>
+#include <stdexcept>
 
-int main(){
+int main(int argc, char **argv){
+    try{
+        RuntimeConfig cfg;
+        parseTempConfig(argc, argv, cfg);
 
+        
+
+    }
+    catch (std::exception &e){
+        std::cerr << "Error:\t" << e.what() << std::endl;
+    }
     return 0;
 }
