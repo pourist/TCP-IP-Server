4af2ec26254b1bdb29b94106befd2b2e5d213b68
ppour-ba <pouriya.pourbahrami@gmail.com>
2025-08-31 19:37:03 +0200

    feat(util): add ansi_colors.hpp with standard terminal color macros

---
+++ include/ansi_colors.hpp
@@ -0,0 +1,18 @@
+// ansi_colors.hpp
+#ifndef ANSI_COLORS_HPP
+#define ANSI_COLORS_HPP
+
+#  define RESET      "\033[0m"
+#  define BOLD       "\033[1m"
+#  define DIM        "\033[2m"
+
+#  define GRAY       "\033[0;90m"
+#  define RED        "\033[0;91m"
+#  define GREEN      "\033[0;92m"
+#  define YELLOW     "\033[0;93m"
+#  define BLUE       "\033[0;94m"
+#  define MAGENTA    "\033[0;95m"
+#  define CYAN       "\033[0;96m"
+
+
+#endif // ANSI_COLORS_HPP
